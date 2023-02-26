0050268ecb10ed6be5fe5f1d8c918da0b3c414f4
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-26 11:57:27 +0900

    Add files via upload

---
+++ JavaScript_practiceI_conditional branch_else if.js
@@ -0,0 +1,10 @@
+const age = 17;
+
+// If the value of age is between 10 and 20, output "I am under 20 and over 10."
+if (age >= 20) {
+  console.log("I am over 20 years old.");
+} else if (age >= 10) {
+  console.log("I am under 20 and over 10.") ;  
+} else {
+  console.log("I am under 10 years old.");
+}
